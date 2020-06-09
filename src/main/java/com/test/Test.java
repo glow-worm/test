9b8f326a7cecc63fae9721c22d1449936e3c759f
wangyulong <Yulong.Wang@verystar.cn>
2020-06-09 22:42:29 +0800

    first commit

---
+++ src/main/java/com/test/Test.java
@@ -0,0 +1,9 @@
+package com.test;
+
+import sun.jvm.hotspot.gc_implementation.parallelScavenge.PSYoungGen;
+
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("111111");
+    }
+}
